d1fd6e03cb4059e2f52e53daaf1a48f4bff4b9e9
Nahuel Medina <nahuel.m.puerari@gmail.com>
2023-08-13 23:45:10 -0300

    agregar div con función

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let div = document.getElementById('soydiv');
+
+    div.addEventListener('click', function(){
+        alert("Hola! Soy el div")
+    });
+});
