50f03d179b81834917eeeef6cf05c95f39ca9d11
Nahuel Medina <139724852+nahuelmedina21@users.noreply.github.com>
2023-09-02 17:39:05 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,7 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     let div = document.getElementById('soydiv');
+    let boton = document.getElementById('boton')
 
     div.addEventListener('click', function(){
         alert("Hola! Soy el div")
     });
 });
+
+function saludar(event) {
+    alert("Hola!");
+    event.stopPropagation();
+  }
